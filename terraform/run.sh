d23a98b852e5b04e771a47cb057e190e2217ef8f
prathibhapadma <90541284+prathibhapadma@users.noreply.github.com>
2021-11-22 13:06:37 +0530

    Create run.sh

---
+++ terraform/run.sh
@@ -0,0 +1,23 @@
+# make sure terraform CLI is installed
+terraform
+
+# format the tf files
+terraform fmt
+
+# initialize terraform Azure modules
+terraform init
+
+# validate the template
+terraform validate
+
+# plan and save the infra changes into tfplan file
+terraform plan
+
+# delete the infra
+#terraform destroy
+
+# cleanup files
+#rm terraform.tfstate
+#rm terraform.tfstate.backup
+#rm .terraform.lock.hcl
+#rm -r .terraform/
