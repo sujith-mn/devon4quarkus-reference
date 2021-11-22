35cb8009c1f528a5992065aa15bfee70fdc61709
prathibhapadma <90541284+prathibhapadma@users.noreply.github.com>
2021-11-22 13:13:04 +0530

    Update run.sh

--- terraform/run.sh
+++ terraform/run.sh
@@ -10,7 +10,7 @@ terraform init
 # validate the template
 terraform validate
 
-# plan and save the infra changes into tfplan file
+# plan and save the infra changes into tfplan file.
 terraform plan
 
 # delete the infra
