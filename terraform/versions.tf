3b8fb83e7fef997e2a86db8082b50103412b1464
prathibhapadma <90541284+prathibhapadma@users.noreply.github.com>
2021-11-22 15:27:06 +0530

    Update versions.tf

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,12 +1,3 @@
-
-variable "appId" {
-  description = "Azure Kubernetes Service Cluster service principal"
-}
-
-variable "password" {
-  description = "Azure Kubernetes Service Cluster password"
-}
-root@Terraform-vm:~/Terraform/AKSClustertf# cat versions.tf
 terraform {
   required_providers {
     azurerm = {
