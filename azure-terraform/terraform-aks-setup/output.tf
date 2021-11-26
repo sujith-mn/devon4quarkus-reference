1df76c8b620135b058996f068df8565535d01e5a
prathibhapadma <90541284+prathibhapadma@users.noreply.github.com>
2021-11-26 10:36:03 +0530

    Create output.tf

---
+++ azure-terraform/terraform-aks-setup/output.tf
@@ -0,0 +1,39 @@
+output "aks_node_rg" {
+  value = azurerm_kubernetes_cluster.aks.node_resource_group
+}
+output "kubernetes_cluster_name" {
+  value = azurerm_kubernetes_cluster.aks.name
+}
+
+output "aks_id" {
+  value = azurerm_kubernetes_cluster.aks.id
+}
+
+output "aks_fqdn" {
+  value = azurerm_kubernetes_cluster.aks.fqdn
+}
+
+output "host" {
+   value = azurerm_kubernetes_cluster.aks.kube_config.0.host
+}
+
+output "client_key" {
+   value = azurerm_kubernetes_cluster.aks.kube_config.0.client_key
+}
+
+output "client_certificate" {
+   value = azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate
+}
+
+output "kube_config" {
+   value = azurerm_kubernetes_cluster.aks.kube_config_raw
+   sensitive = true
+}
+
+output "cluster_username" {
+   value = azurerm_kubernetes_cluster.aks.kube_config.0.username
+}
+
+output "cluster_password" {
+   value = azurerm_kubernetes_cluster.aks.kube_config.0.password
+}
