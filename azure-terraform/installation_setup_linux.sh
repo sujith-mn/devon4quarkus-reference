f37f7bb0a6a68cee1551261ae6d52614cf5ea4be
prathibhapadma <90541284+prathibhapadma@users.noreply.github.com>
2021-12-02 13:25:10 +0530

    updated

--- azure-terraform/installation_setup_linux.sh
+++ azure-terraform/installation_setup_linux.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
-  echo "Install Azure CLI...."
+        echo "Install Azure CLI...."
 	#Get packages needed for the install process:
 	sudo apt-get update
-    sudo apt-get install ca-certificates curl apt-transport-https lsb-release gnupg
+        sudo apt-get install ca-certificates curl apt-transport-https lsb-release gnupg
 	
 	#Download and install the Microsoft signing key:
 	curl -sL https://packages.microsoft.com/keys/microsoft.asc |
-    gpg --dearmor |
-    sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
+        gpg --dearmor |
+        sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
 	
 	#Add the Azure CLI software repository
 	AZ_REPO=$(lsb_release -cs)
-    echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" |
-    sudo tee /etc/apt/sources.list.d/azure-cli.list
+        echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" |
+        sudo tee /etc/apt/sources.list.d/azure-cli.list
 	
 	#Update repository information and install the azure-cli package
 	sudo apt-get update
-    sudo apt-get install azure-cli
+        sudo apt-get install azure-cli
     
 	echo "az version"
 	az --version
 
-  echo "Install the Kubectl...."
+        echo "Install the Kubectl...."
 	
 	az aks install-cli
 	kubectl version --client
@@ -31,7 +31,7 @@
 	#az aks get-credentials --resource-group myResourceGroup --name myAKSCluster
 	#kubectl get nodes
 	
- echo "Install helm package..."
+        echo "Install helm package..."
 	curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
 	sudo apt-get install apt-transport-https --yes
 	echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
@@ -39,14 +39,15 @@
 	sudo apt-get install helm
 	sudo helm version
 	
-	#terraform Installation
- echo "Install Terraform..."
+	
+        echo "Install Terraform..."
 	sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
 	curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 	sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 	sudo apt-get update && sudo apt-get install terraform
 	
-	#terraform -help
+	echo "terraform Version"
 	terraform version
+	#terraform -help
 	
 	
